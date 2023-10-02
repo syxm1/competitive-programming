6ee584c9e1175de64ea5c769b1c54eca18a36fa8
syxm1 <106479331+syxm1@users.noreply.github.com>
2023-10-02 21:56:34 +0700

    Add files via upload

---
+++ online-judge/cses/sorting-and-searching/restaurant-customers.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define int     long long
+#define ull     unsigned long long
+#define ld      long double
+#define pii     pair<int, int>
+#define str     string
+#define fi      first
+#define se      second
+// #define oset    tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define endl    '\n'
+
+#define len     length
+#define pb      push_back
+#define ppb     pop_back
+#define pf      push_front
+#define ppf     pop_front
+#define lb      lower_bound
+#define ub      upper_bound
+
+#define LSOne(x)   ((x) & (-x))
+#define sz(x)	    (int) x.size()
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define ppc(x)     __builtin_popcount(x)
+#define ppcll(x)   __builtin_popcountll(x)
+
+#define mod     1000000007 // 1e9+7
+#define eps     0.000000001 // 1e-9
+#define inf     2147483647 // INT_MAX
+#define INF     9223372036854775807 // LLONG_MAX
+// const vector<int> dx = {1,-1,0,0,1,-1,-1,1};
+// const vector<int> dy = {0,0,-1,1,-1,-1,1,1};
+
+template<typename T> void print(T var, string end = "") {cout << var << end;}
+template<typename T> void print(vector<T> var, string end = "") {cout << '['; for(int i = 0; i < var.size(); i++) {print(var[i], (i == var.size() - 1 ? "" : ", "));} cout << ']' << end;}
+template<typename T> void print(vector<vector<T>> var, string end = "\n") {for(auto &i : var) {print(i, end);}}
+template<typename T, typename U> void print(pair<T, U> var, string end = "") {cout << '[' << var.first << ", " << var.second << ']' << end;}
+template<typename T, typename U> void print(vector<pair<T, U>> var, string end = "\n") {for(auto &i : var) {print(i, end);}}
+template<typename T, typename U> void print(map<T, U> var, string end = "\n") {for(auto &i : var) {print(i, end);}}
+
+void print() {cout << "\n";}
+template <typename T, typename... V> void print(T t, V... v) {print(t); if (sizeof...(v)) cout << ", "; print(v...);}
+#define trace(x...) cout << #x << " : " ; print(x); cout << endl
+
+void Main(int tc) { 
+    int n;
+    cin >> n;
+
+    vector<pii> a(n);
+    vector<int> c;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i].fi >> a[i].se;
+        c.pb(a[i].fi);
+        c.pb(a[i].se);
+    }
+
+    sort(all(c));
+    c.erase(unique(all(c)), c.end());
+    
+    map<int, int> id;
+    for(int i = 0; i < c.size(); i++) {
+        id[c[i]] = i;
+        // cout << c[i] << " \n"[i == c.size() - 1];
+    }
+
+    vector<int> pref(2 * n + 5, 0);
+
+    // print(a);
+    for(int i = 0; i < n; i++) {
+        pref[id[a[i].fi]]++;
+        pref[id[a[i].se]+1]--;
+    }
+
+    // print(id);
+
+    for(int i = 1; i < 2 * n + 5; i++) {
+        pref[i] = pref[i-1] + pref[i];
+    }
+
+    // print(pref, "\n");
+    cout << *max_element(all(pref)) << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 1;
+    //cin >> t; 
+
+    for(int tc = 1; tc <= t; tc++) {
+        Main(tc);
+    }
+}
+
+/*
+
+*/
