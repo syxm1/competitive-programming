294ce6238d1738d70451bdc315556ab7b610866d
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-04-21 11:22:39 +0700

    Update checker.bat

--- algorithm/stress-testing/checker.bat
+++ algorithm/stress-testing/checker.bat
@@ -1,24 +1,24 @@
 @echo off
 D:
-cd \Workspace\Competitive Programming 
-:: path
+cd \workspace\competiitve-programming
 
-g++ -std=c++17 test.cpp -o  test
-:: test case generator
+g++ -std=c++17 soln.cpp -o soln
+g++ -std=c++17 brute.cpp -o brute
+g++ -std=c++17 test.cpp -o test
 
-g++ -std=c++17 soln.cpp -o  soln
-:: wrong solutioin
-
-g++ -std=c++17 brute.cpp -o  brute 
-:: correct solution
 
 for /l %%x in (1, 1, 100) do (
-    test > input.in
-    soln < input.in > output.out 
-    brute < input.in > output2.out
-    fc output.out output2.out > diagnostics || (echo Test %%x [1m[31mFAILED[0m & echo, & echo [1m[36mFailed Test:[0m & type input.in  & echo, & echo [1m[32mCorrect Answer:[0m  & type output2.out & echo,  & echo [1m[31mYour Answer:[0m  & type output.out & exit /b)
-    echo Test %%x [1m[32mPASSED[0m
+	test > input.in
+	soln < input.in > a.out
+	brute < input.in > b.out
+	fc a.out b.out > c || (
+		echo correct:
+		type b.out
+		echo,  
+		echo wrong: 
+		type a.out 
+		exit /b
+	)
 )
 
-echo all generated tests [1m[32mPASSED[0m
-@REM pause
+echo selesai
