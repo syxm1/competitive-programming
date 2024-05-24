12fb5f9e0a593cecd98e37de9d093dade8a634fc
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-05-24 21:47:04 +0700

    Update code.cpp

--- algorithm/data-structure/pbds/code.cpp
+++ algorithm/data-structure/pbds/code.cpp
@@ -1,2 +1,11 @@
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
+
+template<typename T> using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using ordered_multiset =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using decreasing_ordered_set =
+    tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using decreasing_ordered_multiset =
+    tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
