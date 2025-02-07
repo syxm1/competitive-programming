433d4ec704a5bf743e64b4ccfbf264f115839d21
syxm1 <hisyamprsetyo@gmail.com>
2025-02-08 05:30:38 +0700

    adding lazy segtree implementation

---
+++ online-judge/codeforces/2063/b.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        int n, l, r;
+        cin >> n >> l >> r;
+
+        vector<int> a(n);
+        
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        
+    }
+}
