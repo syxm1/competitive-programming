a5243d867f09d62c49385a199d074809363a4483
syxm1 <hisyamprsetyo@gmail.com>
2025-03-27 18:14:42 +0700

    tidying things

--- online-judge/oj/NOI18_knapsack.cpp
+++ online-judge/oj/NOI18_knapsack.cpp
@@ -5,7 +5,7 @@
 - do the classic knapsack dp.
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
@@ -18,47 +18,49 @@ const int S = 2000;
 ll s, n, v, w, k, id, cap;
 vector<vector<ll>> dp;
 vector<pair<ll, ll>> arr;
-vector<vector<pair<ll,ll>>> a;
+vector<vector<pair<ll, ll>>> a;
 
 signed main() {
-	cin >> s >> n;
+  cin >> s >> n;
 
-	a.resize(S+5);
+  a.resize(S + 5);
 
-	for (int i = 0; i < n; i++) {
-		cin >> v >> w >> k;
-		a[w].push_back({v, k});
-	}
+  for (int i = 0; i < n; i++) {
+    cin >> v >> w >> k;
+    a[w].push_back({v, k});
+  }
 
-	for (int i = 1; i < S; i++) {
-		sort(a[i].rbegin(), a[i].rend());
-		cap = S / i, id = 0;
-		while (cap > 0 && id < (int) a[i].size()) {
-			for (int j = 0; j < min(cap, a[i][id].se); j++) {
-				arr.push_back({i, a[i][id].fi});
-			}
-			cap -= min(cap, a[i][id].se), id++;
-		}
-	}
+  for (int i = 1; i < S; i++) {
+    sort(a[i].rbegin(), a[i].rend());
+    cap = S / i, id = 0;
+    while (cap > 0 && id < (int)a[i].size()) {
+      for (int j = 0; j < min(cap, a[i][id].se); j++) {
+        arr.push_back({i, a[i][id].fi});
+      }
+      cap -= min(cap, a[i][id].se), id++;
+    }
+  }
 
-	dp.assign((int) arr.size(), vector<ll>(s+5));
+  dp.assign((int)arr.size(), vector<ll>(s + 5));
 
-	for (int i = 0; i <= s; i++) {
-		if (i >= arr[0].fi) dp[0][i] = arr[0].se;
-	}
+  for (int i = 0; i <= s; i++) {
+    if (i >= arr[0].fi)
+      dp[0][i] = arr[0].se;
+  }
 
-	for (int i = 1; i < (int) arr.size(); i++) {
-		for (int j = 0; j <= s; j++) {
-			dp[i][j] = dp[i-1][j];
-			if (j >= arr[i].fi) dp[i][j] = max(dp[i][j], dp[i-1][j-arr[i].fi] + arr[i].se);
-		}
-	}
+  for (int i = 1; i < (int)arr.size(); i++) {
+    for (int j = 0; j <= s; j++) {
+      dp[i][j] = dp[i - 1][j];
+      if (j >= arr[i].fi)
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - arr[i].fi] + arr[i].se);
+    }
+  }
 
-	ll ans = 0;
+  ll ans = 0;
 
-	for (int i = 0; i <= s; i++) {
-		ans = max(ans, dp[arr.size()-1][i]);
-	}
+  for (int i = 0; i <= s; i++) {
+    ans = max(ans, dp[arr.size() - 1][i]);
+  }
 
-	cout << ans << '\n';
+  cout << ans << '\n';
 }
